0f92ffc379f430421753cdf86da6c3e3f1d835a5
AISoftMaster <ormonkenenbaev@gmail.com>
2021-03-12 18:02:42 +0600

    homework50

--- TODO_list/listapp/models.py
+++ TODO_list/listapp/models.py
@@ -5,11 +5,25 @@ class Type(models.Model):
     name = models.CharField(max_length=350)
     def __str__(self):
         return self.name
+
+
 class Status(models.Model):
     name = models.CharField(max_length=350)
+
     def __str__(self):
         return self.name
 # Create your models here.
+
+
+class TaskType(models.Model):
+    task = models.ForeignKey('listapp.Task', related_name="task_types", on_delete=models.CASCADE, verbose_name='Задача')
+
+    type = models.ForeignKey('listapp.Type', related_name="type_tasks", on_delete=models.CASCADE, verbose_name='Тэг')
+
+    def __str__(self):
+        return "{} | {}".format(self.task, self.type)
+
+
 class Task(models.Model):
     title = models.CharField(max_length=350)
 
@@ -18,14 +32,10 @@ class Task(models.Model):
     upgreate = models.DateTimeField(auto_now=True)
 
     # type = models.ForeignKey('listapp.Type', on_delete=models.PROTECT, related_name="tasks")
+    type = models.ManyToManyField('listapp.Type', through='listapp.TaskType', through_fields=('task', 'type'), related_name="tasks")
     status = models.ForeignKey('listapp.Status', on_delete=models.PROTECT, related_name="status")
 
     def __str__(self):
         return self.title
 
-class TaskType(models.Model):
-    task = models.ForeignKey('listapp.Task',related_name="task_types", on_delete=models.CASCADE, verbose_name='время создания')
 
-    type = models.ForeignKey('listapp.Type',related_name="type_tasks", on_delete=models.CASCADE, verbose_name='Тэг')
-    def __str__(self):
-        return "{} | {}".format(self.task, self.type)
