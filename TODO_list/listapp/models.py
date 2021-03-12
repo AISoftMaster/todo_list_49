3b2506c39559d761c7c5063398d1caac11715fd5
AISoftMaster <ormonkenenbaev@gmail.com>
2021-03-12 16:51:01 +0600

    first

--- TODO_list/listapp/models.py
+++ TODO_list/listapp/models.py
@@ -13,14 +13,19 @@ class Status(models.Model):
 class Task(models.Model):
     title = models.CharField(max_length=350)
 
-    description = models.CharField(max_length=2048, null=True, blank=True)
+    description = models.CharField(max_length=2048, null=False, blank=False, verbose_name="Описание")
     created = models.DateTimeField(auto_now_add=True)
     upgreate = models.DateTimeField(auto_now=True)
 
-    type = models.ForeignKey('listapp.Type', on_delete=models.PROTECT, related_name="tasks")
+    # type = models.ForeignKey('listapp.Type', on_delete=models.PROTECT, related_name="tasks")
     status = models.ForeignKey('listapp.Status', on_delete=models.PROTECT, related_name="status")
 
     def __str__(self):
         return self.title
 
+class TaskType(models.Model):
+    task = models.ForeignKey('listapp.Task',related_name="task_types",on_delete=models.CASCADE, verbose_name='время создания')
 
+    type = models.ForeignKey('listapp.Type',related_name="type_tasks",on_delete=models.CASCADE, verbose_name='Тэг')
+    def __str__(self):
+        return "{} | {}".format(self.task, self.type)
